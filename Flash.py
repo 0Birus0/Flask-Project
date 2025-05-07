255eb6fb08fda5e2e27a10bd187ea2e7525c05e7
Anish <anishnagelli@gmail.com>
2025-05-07 19:57:01 -0400

    Start of project

---
+++ Flash.py
@@ -0,0 +1,52 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hey!</h1>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
